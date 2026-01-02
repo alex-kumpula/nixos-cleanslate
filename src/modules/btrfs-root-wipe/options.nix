a9055d5ce23029811e5f33673a49cb8b82fec9ef
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-02 00:33:24 -0700

    Start options

--- src/modules/btrfs-root-wipe/options.nix
+++ src/modules/btrfs-root-wipe/options.nix
@@ -1,14 +1,14 @@
 { inputs, config, lib, ... }:
 {
   flake.modules.nixos.btrfs-root-wipe =
-    { lib, ... }:
-    {
-      options.btrfs-root-wipe = {
-        mainDisk = lib.mkOption {
-          type = lib.types.str;
-          default = "/dev/vda";
-          description = "The main disk device.";
-        };
+  { lib, ... }:
+  {
+    options.btrfs-root-wipe = {
+      mainDisk = lib.mkOption {
+        type = lib.types.str;
+        default = "/dev/vda";
+        description = "The main disk device.";
       };
     };
+  };
 }
