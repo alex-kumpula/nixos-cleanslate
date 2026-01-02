c0dcb6187897e3172c3883da31441b30ff2a70bb
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-01 18:12:38 -0700

    Update

--- src/nixosConfigurations/example-host/example-host.nix
+++ src/nixosConfigurations/example-host/example-host.nix
@@ -1,18 +1,10 @@
 { inputs, ... }:
 {
-  flake.nixosConfigurations.alex-vm-001 = inputs.nixpkgs.lib.nixosSystem {
+  flake.nixosConfigurations.example-host = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = with inputs.self.modules.nixos; [ 
-      # ./_disk_config.nix
-      # ./_persistence.nix
-      vm-configuration 
-      vm-hardware-configuration
-      git
-      alex-vm-001
+      example-host
       btrfs-impermanence
-    ] ++ [
-      # inputs.disko.nixosModules.default
-      # inputs.impermanence.nixosModules.impermanence
     ];
   };
 }
