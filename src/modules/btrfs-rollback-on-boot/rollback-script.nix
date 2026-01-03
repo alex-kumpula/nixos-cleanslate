1d5241c9990c7ae24ddea1dbb19d6eb9b4290c71
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-02 18:30:02 -0700

    test

--- src/modules/btrfs-rollback-on-boot/rollback-script.nix
+++ src/modules/btrfs-rollback-on-boot/rollback-script.nix
@@ -11,8 +11,7 @@
   flake.modules.nixos.btrfs-rollback-on-boot =
   { lib, pkgs, config, ... }:
   let 
-      # cfg = config.btrfs-rollback-on-boot;
-      cfg = lib.getAttrFromPath [ "btrfs-rollback-on-boot" ] config;
+      cfg = config.btrfs-rollback-on-boot;
 
       rollbackScriptContent = builtins.readFile ./rollback.sh;
   in
@@ -20,15 +19,42 @@
   {
     config = lib.mkIf cfg.enable {
 
-      btrfs-rollback-on-boot = {
-        rollbackServiceScripts = lib.mapAttrs' (
-          name: serviceCfg: lib.nameValuePair name (
+      # btrfs-rollback-on-boot = {
+      #   rollbackServiceScripts = lib.mapAttrs' (
+      #     name: serviceCfg: lib.nameValuePair name (
             
-            pkgs.writeShellScriptBin "rollback-${name}" ''
+      #       pkgs.writeShellScriptBin "rollback-${name}" ''
+      #         #
+      #         # --- Variables interpolated by Nix ---
+      #         #
+
+      #         SV_WIPE_DEVICE="${serviceCfg.subvolumeToWipe.device}"
+      #         SV_WIPE_PATH_ON_DEVICE="${serviceCfg.subvolumeToWipe.path}"
+      #         SV_WIPE_NAME="${serviceCfg.subvolumeToWipe.name}"
+      #         SV_WIPE_DEVICE_TEMP_MOUNT_POINT="/subvolume-$SV_WIPE_NAME-mount_dir"
+              
+      #         SV_PERSIST_DEVICE="${serviceCfg.subvolumeForPersistence.device}"
+      #         SV_PERSIST_PATH_ON_DEVICE="${serviceCfg.subvolumeForPersistence.path}"
+      #         SV_PERSIST_NAME="${serviceCfg.subvolumeForPersistence.name}"
+      #         SV_PERSIST_DEVICE_TEMP_MOUNT_POINT="/subvolume-$SV_PERSIST_NAME-mount_dir"
+
+      #         SNAPSHOT_PATH_IN_SV_PERSIST="/snapshots"
+
+      #       '' + rollbackScriptContent
+      #     )
+
+      #   ) cfg.services;
+      # };
+
+      btrfs-rollback-on-boot.rollbackServiceScripts = lib.mapAttrs' (
+        name: serviceCfg:
+          let
+            # 1. Define the package explicitly in an isolated let binding.
+            # This makes the derivation result clear and atomic.
+            scriptPackage = pkgs.writeShellScriptBin "rollback-${name}" ''
               #
               # --- Variables interpolated by Nix ---
               #
-
               SV_WIPE_DEVICE="${serviceCfg.subvolumeToWipe.device}"
               SV_WIPE_PATH_ON_DEVICE="${serviceCfg.subvolumeToWipe.path}"
               SV_WIPE_NAME="${serviceCfg.subvolumeToWipe.name}"
@@ -40,12 +66,11 @@
               SV_PERSIST_DEVICE_TEMP_MOUNT_POINT="/subvolume-$SV_PERSIST_NAME-mount_dir"
 
               SNAPSHOT_PATH_IN_SV_PERSIST="/snapshots"
-
-            '' + rollbackScriptContent
-          )
-
-        ) cfg.services;
-      };
+            '' + rollbackScriptContent;
+          in
+          # 2. Return the named pair with the scriptPackage variable.
+          lib.nameValuePair name scriptPackage
+      ) cfg.services;
 
       # 2. Extract the generated scripts and add them to extraBin
       # We use lib.mapAttrs to transform the attribute set into the required format.
