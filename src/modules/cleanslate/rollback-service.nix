c2005bc4769b41946bd66e29056e00c4fcd2f06e
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-11 22:07:37 -0700

    test

--- src/modules/cleanslate/rollback-service.nix
+++ src/modules/cleanslate/rollback-service.nix
@@ -12,9 +12,14 @@
         name: serviceCfg: lib.nameValuePair "${name}" {
           
           wantedBy = ["initrd-root-device.target"];
-          wants = ["lvm2-activation.service"];
+          # wants = ["lvm2-activation.service"];
           # See https://github.com/nix-community/impermanence/issues/250#issuecomment-2603848867
-          after = ["lvm2-activation.service" "local-fs-pre.target" "cryptsetup.target"];
+          # after = ["lvm2-activation.service" "local-fs-pre.target" "cryptsetup.target"];
+          # before = ["sysroot.mount"];
+          after = [
+            "local-fs-pre.target"
+            "initrd-root-fs.target"
+          ];
           before = ["sysroot.mount"];
           # Run on cold boot only, never on resume from hibernation
           unitConfig = {
