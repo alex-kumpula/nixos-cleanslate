7825a7b327e8b1627bbc59d31c3aabb112e6cf1a
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-13 18:53:34 -0700

    test

--- src/modules/cleanslate/rollback-service.nix
+++ src/modules/cleanslate/rollback-service.nix
@@ -11,11 +11,35 @@
       boot.initrd.systemd.services = lib.mapAttrs' (
         name: serviceCfg: lib.nameValuePair "${name}" {
           
-          wantedBy = ["initrd-root-device.target" "initrd.target"];
-          # wants = ["lvm2-activation.service"];
-          # See https://github.com/nix-community/impermanence/issues/250#issuecomment-2603848867
-          after = ["lvm2-activation.service" "local-fs-pre.target" "cryptsetup.target"];
-          before = ["sysroot.mount"];
+          # wantedBy = ["initrd-root-device.target" "initrd.target"];
+          # # wants = ["lvm2-activation.service"];
+          # # See https://github.com/nix-community/impermanence/issues/250#issuecomment-2603848867
+          # after = ["lvm2-activation.service" "local-fs-pre.target" "cryptsetup.target"];
+          # before = ["sysroot.mount"];
+
+
+
+          enableStrictShellChecks = false;
+
+          wantedBy = [ "initrd-root-device.target" ];
+
+          wants = [
+            "cryptsetup.target"
+            "lvm2-activation.service"
+          ];
+
+          after = [
+            "cryptsetup.target"
+            "systemd-cryptsetup@cryptroot.service"
+            "lvm2-activation.service"
+            "local-fs-pre.target"
+          ];
+
+          before = [ "sysroot.mount" ];
+
+
+
+
           # Run on cold boot only, never on resume from hibernation
           unitConfig = {
             ConditionKernelCommandLine = ["!resume="];
